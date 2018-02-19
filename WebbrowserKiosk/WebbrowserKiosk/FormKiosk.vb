1e23619bf1419c5f48015420e7f2ca6b091ed827
Tim Gremalm <tim@gremalm.se>
2018-02-19 22:49:38 +0100

    Buffer input keys from webbrowser and navigate

--- WebbrowserKiosk/WebbrowserKiosk/FormKiosk.vb
+++ WebbrowserKiosk/WebbrowserKiosk/FormKiosk.vb
@@ -1,5 +1,27 @@
-﻿Public Class FormKiosk
+﻿Imports System.Text.RegularExpressions
+
+Public Class FormKiosk
+	Private InputBuffer As String = ""
+
 	Private Sub FormKiosk_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+	End Sub
+
+	Private Sub WebBrowser1_PreviewKeyDown(sender As Object, e As PreviewKeyDownEventArgs) Handles WebBrowser1.PreviewKeyDown
+		Dim KeyString As String = Chr(e.KeyCode)
+
+		'If digit, add to buffer
+		Dim Pattern As String = "^[0-9]+$"
+		Dim r As Regex = New Regex(Pattern, RegexOptions.IgnoreCase)
+		Dim m As Match = r.Match(KeyString)
+		If m.Success Then
+			InputBuffer &= KeyString
+		End If
+
+		'If enter, navigate to URL
+		If e.KeyCode = Keys.Return Then
+			WebBrowser1.Navigate("http://192.168.0.1:8000/profile/" & InputBuffer)
+			InputBuffer = ""
+		End If
 	End Sub
 End Class
